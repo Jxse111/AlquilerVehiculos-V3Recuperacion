73e3a60722741328d4c1d0556f55eae9982c646e
José <Jos@DESKTOP-J0SE>
2023-06-12 10:55:59 +0200

    .

--- AlquilerVehiculos-v3/src/main/java/vista/utilidades/Main.java
+++ AlquilerVehiculos-v3/src/main/java/vista/utilidades/Main.java
@@ -1,13 +1,28 @@
 package vista.utilidades;
 
 import javafx.application.Application;
-import vista.recursos.LanzadorVentanaPrincipal;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-Application.launch(LanzadorVentanaPrincipal.class,args);
+public class Main extends Application {
+	
+	@Override
+	public void start(Stage escenarioPrincipal) {
+		try {
+			Parent raiz = FXMLLoader.load(getClass().getResource("panelPrincipal.fxml"));
+			
+			Scene escena = new Scene(raiz);
+			escenarioPrincipal.setTitle("Hola mundo desde JavaFX");
+			escenarioPrincipal.setScene(escena);
+			escenarioPrincipal.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
