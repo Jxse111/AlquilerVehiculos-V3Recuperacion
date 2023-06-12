be6042de620212fd60528c70c00caf7ecb315e05
José <Jos@DESKTOP-J0SE>
2023-06-12 14:12:50 +0200

    .

---
+++ AlquilerVehiculos-v3/src/main/java/vista/utilidades/MainApp.java
@@ -0,0 +1,13 @@
+package vista.utilidades;
+
+import javafx.application.Application;
+import vista.recursos.LanzadorVentanaPrincipal;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Application.launch(LanzadorVentanaPrincipal.class,args);
+	}
+
+}
